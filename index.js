cf586f922845056df1a74efd117d5552accee708
Yago <almeidayago167@gmail.com>
2024-03-19 18:30:15 -0400

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        console.log(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
